68668dd52771657994b90780e3a414415f92dcc8
Felipe Pupo <felipepupo@outlook.com.br>
2023-01-24 11:52:56 -0300

    First commit - Spring boot applicantion with JDBC and JPA

---
+++ src/main/java/com/example/springbootjdbcjpa/SpringbootJdbcJpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootjdbcjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJdbcJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootJdbcJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springbootjdbcjpa/SpringbootJdbcJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootjdbcjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootJdbcJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
